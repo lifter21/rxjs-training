082233d293f7d8b8a9d4214030c9c99e650355da
Oleksandr <bondarenko.alexander.13@gmail.com>
2017-06-22 17:50:43 +0300

    Add simple observable

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Observable, Observer } from 'rxjs/Rx';
 
 let numbers = [1,5,10];
-let source = Observable.from(numbers);
+// let source = Observable.from(numbers);
 
 class MyObserver implements Observer<number> {
 
@@ -17,6 +17,15 @@ class MyObserver implements Observer<number> {
         console.log('complete');
     }
 }
+
+let source = Observable.create(observer => {
+    for(let n of numbers) {
+        observer.next(n);
+    }
+
+    observer.complete();
+});
+
 
 // source.subscribe(new MyObserver());
 
