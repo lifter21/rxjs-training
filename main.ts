437429020ed39e0bcf2f8fc6f63cb795a81b05a9
Oleksandr <bondarenko.alexander.13@gmail.com>
2017-06-26 12:53:43 +0300

    add fetching and processing of movies.json

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import { Observable, Observer } from 'rxjs/Rx';
-
-import * as $ from 'jquery'
+import * as $ from 'jquery';
 
 let numbers = [1, 5, 10];
 // let source = Observable.from(numbers);
@@ -40,7 +39,7 @@ let source_1 = Observable.create(observer => {
     .map(n => n * 2)
     .filter(n => n > 5);
 
-// source.subscribe(new MyObserver());
+// source_1.subscribe(new MyObserver());
 
 // source_1.subscribe(
 //    value => console.log(`value: ${value}`),
@@ -71,7 +70,7 @@ function onNext(val) {
     // circle.style.left = val.x;
     // circle.style.top = val.y;
 
-    $circle.css({left:  val.x, top: val.y})
+    $circle.css({ left: val.x, top: val.y })
 }
 
 // source_2.subscribe(
@@ -80,17 +79,96 @@ function onNext(val) {
 //     () => console.log('completed')
 // );
 
+const getMoviesBtn = document.getElementById('getMoviesBtn');
+const movieList = document.getElementById('movies');
+
+const moviesPath = 'movies.json'
+
+let click = Observable.fromEvent(getMoviesBtn, 'click')
+
+interface Movie {
+    title: string;
+    [propName: string]: any;
+}
+
+const retryStrategy = ({ attempts = 3, delay = 1000 } = {}) => {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+                return acc += 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
+}
+
+const load = (path: string) => {
+    return Observable.create(observer => {
+        const xhr = new XMLHttpRequest();
+
+
+        // return Observable.fromEvent(xhr, 'load')
+        //     .map(...)
+
+        xhr.addEventListener('load', () => {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        });
+
+        xhr.open('GET', path);
+        xhr.send();
+    })
+        // .retry(3)
+        .retryWhen(retryStrategy({ attempts: 5, delay: 2000 }));
+};
+
+function loadWithFetch(path: string): Observable<Movie[]> {
+    return Observable
+        .defer(() => {
+            return Observable.fromPromise(fetch(path).then(data => data.json()));
+        })
+        .retryWhen(retryStrategy({ attempts: 5, delay: 2000 }));
+};
+
+const renderMovies = (movies: Movie[]) => {
+    movies.forEach(movie => {
+        appendListItem(movie.title, movieList);
+    });
+};
+
+// loadMovies(moviesPath).subscribe(renderMovies);
+
+click
+    .flatMap(e => loadWithFetch(moviesPath))
+    .subscribe(
+    renderMovies,
+    err => console.error(err),
+    () => console.log('completed')
+    )
+
 /**
  *
  * Rxjs book
  *
  */
 
+function appendListItem(value, parentNode) {
+    let node = document.createElement('li');                 // Create a <li> node
+    let textnode = document.createTextNode(value);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    parentNode.appendChild(node);
+}
+
 let input = document.getElementById('input'),
     results = document.getElementById('results');
 
 let keyups = Observable.fromEvent(input, 'keyup')
-    .map((e : KeyboardEvent) => (<HTMLInputElement>e.target).value)
+    .map((e: KeyboardEvent) => (<HTMLInputElement>e.target).value)
     .filter(text => text.length > 2);
 
 let throttled = keyups.throttleTime(500);
@@ -115,23 +193,16 @@ const clearResults = () => {
     results.innerHTML = '';
 }
 
-const appendListItem = value => {
-    let node = document.createElement('li');                 // Create a <li> node
-    let textnode = document.createTextNode(value);         // Create a text node
-    node.appendChild(textnode);                              // Append the text to <li>
-    results.appendChild(node);
-}
-
-suggestions.subscribe(
-    data => {
-        // console.log(data);
-        const res = data[1];
-        clearResults();
-        res.forEach(appendListItem);
-    },
-    error => {
-        console.log(error);
-        clearResults();
-    },
-    () => console.log('finished')
-)
+// suggestions.subscribe(
+//     data => {
+//         // console.log(data);
+//         const res = data[1];
+//         clearResults();
+//         res.forEach(r => appendListItem(r, results));
+//     },
+//     error => {
+//         console.log(error);
+//         clearResults();
+//     },
+//     () => console.log('finished')
+// );
